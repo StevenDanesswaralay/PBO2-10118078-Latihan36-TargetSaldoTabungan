00810aafba5870f90a07b489d2f60462d735ddf6
Steven Danesswaralay <55594713+StevenDanesswaralay@users.noreply.github.com>
2019-10-17 09:43:22 +0700

    Add files via upload

---
+++ src/main/java/com/mycompany/pbo2/latihan36/targetsaldotabungan/Tabungan.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.pbo2.latihan36.targetsaldotabungan;
+
+/**
+ *
+ * @author 
+ * Nama     : Steven Danesswaralay
+ * Kelas    : PBO2
+ * Nim      : 10118078
+ * Deskripsi Tugas : Target Saldo Tabungan
+ */
+public class Tabungan {
+    private int saldoTarget;
+    private int saldo;
+    private double bunga;
+
+    public void setSaldoTarget(int saldoTarget) {
+        this.saldoTarget = saldoTarget;
+    }
+
+    public void setSaldo(int saldo) {
+        this.saldo = saldo;
+    }
+
+    public void setBunga(double bunga) {
+        this.bunga = bunga;
+    }
+    
+    public void hitungTarget(){
+        int i = 0;
+        while(saldo <= saldoTarget){
+            i++;
+            saldo = saldo + (int)(saldo * bunga);
+            System.out.println("Saldo di bulan ke-" + i + " Rp. "+
+                    String.format("%,d", saldo).replace(',','.'));
+        }
+    }
+}
